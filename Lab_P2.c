333626686146c86145b216d8935920f703e9b4e3
pavanganeshpg <pavanganesh9949@gmail.com>
2025-05-19 00:24:26 +0530

    added Lab_p3.c

--- Lab_P2.c
+++ Lab_P2.c
@@ -1,39 +1,48 @@
-# include<stdio.h>
-# include<stdbool.h>
+#include <stdio.h>
+#include <stdbool.h>
 
-struct Interval{
+struct Interval
+{
     int start, end;
 };
-int compareInterval(const void* a, const void* b)
+int compareInterval(const void *a, const void *b)
 {
-    return ((struct Interval*)a)->start - ((struct Interval*)b)->start;
+    return ((struct Interval *)a)->start - ((struct Interval *)b)->start;
 }
 void Merge(struct Interval arr[], int n)
 {
-   if(n <= 0)
-     return;
+    if (n <= 0)
+        return;
 
-   qsort(arr, n, sizeof(struct Interval), compareInterval);
-   int idx = 0;
-   for(int i =1; i<n; i++){
-    if (arr[idx].end >= arr[idx].start){
-        if (arr[idx].end < arr[i].end)
-           arr[idx].end = arr[i].end;
+    qsort(arr, n, sizeof(struct Interval), compareInterval);
+    int idx = 0;
+    for (int i = 1; i < n; i++)
+    {
+        // If current interval overlaps with the previous, merge them
+        if (arr[idx].end >= arr[i].start)
+        {
+            if (arr[idx].end < arr[i].end)
+                arr[idx].end = arr[i].end;
+        }
+        else
+        {
+            idx++;
+            arr[idx] = arr[i];
+        }
     }
-    else{
-        idx++;
-        arr[idx] = arr[i];
+    printf("The Merged Intervals are: ");
+    for (int i = 0; i <= idx; i++)
+    {
+        printf("[%d,%d]", arr[i].start, arr[i].end);
+        if (i < idx)
+            printf(" ");
     }
-}
-printf("The Merged Intervals are:");
-for (int i = 0; i<= idx; i++){
-    printf("[%d,%d]", arr[i].start, arr[i].end);
-}
+    printf("\n");
 }
 int main()
 {
-    struct Interval intervals[] = {{2,3}, {1,4}};
-    int n = sizeof(intervals)/ sizeof(intervals[0]);
+    struct Interval intervals[] = {{2, 3}, {1, 4}};
+    int n = sizeof(intervals) / sizeof(intervals[0]);
     Merge(intervals, n);
     return 0;
 }
